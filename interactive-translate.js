b6ef3de3f0ed2a57b6fb1a0ed5f565c10e92c7be
Joshua Byrd <phocks@gmail.com>
2019-10-04 14:42:24 +1000

    Translations

--- interactive-translate.js
+++ interactive-translate.js
@@ -14,31 +14,50 @@ for (let file of files) {
 
   const parsedYaml = YAML.parse(text);
 
+  let logFilename = false;
+
   traverse(parsedYaml).forEach(function(element) {
     if (element && element.text) {
       // console.log(element);
       // this.node.text.vi = "TEST"
       // console.log(this.node);
       const englishText = element.text.en;
-      const foundChoice = reference.find(el => el.en === englishText);
-      
+      const foundChoice = reference.find(el => {
+        // el.en.toLowerCase() === englishText.toLowerCase()
+        const firstCompare = el.en.toLowerCase().replace(/[^a-z0-9\ ]/gi, "");
+        const secondCompare = englishText
+          .toLowerCase()
+          .replace(/[^a-z0-9\ ]/gi, "");
+
+        return firstCompare === secondCompare;
+      });
+
       // console.log(element.text[OTHER_LANGUAGE])
 
+      const otherLang = this.node.text[OTHER_LANGUAGE];
+
       if (foundChoice) {
-        if (!this.node.text[OTHER_LANGUAGE]) {
+        if (!otherLang) {
           this.node.text[OTHER_LANGUAGE] = foundChoice[OTHER_LANGUAGE];
+          logFilename = true;
+          console.log(foundChoice);
         }
-        
+
         // if (typeof element.text[OTHER_LANGUAGE] !== "undefined") {
         //   console.log(element.text[OTHER_LANGUAGE]);
         // }
       }
 
-      console.log(element.text)
+      // console.log(element.text)
     }
   });
 
+  if (logFilename) console.log("^^^^from " + file);
+  logFilename = false;
+
   // console.log(YAML.stringify(parsedYaml));
+
+  fs.writeFileSync("./out/" + file, YAML.stringify(parsedYaml));
 
   // const choices = parsedYaml.choice_groups[0].choices;
 
